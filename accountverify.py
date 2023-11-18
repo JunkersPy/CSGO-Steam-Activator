57c1303e39085f833e716e40fc3c034505c045fc
Junkie <83054378+JunkersPy@users.noreply.github.com>
2023-11-18 17:31:35 -0500

    Update accountverify.py
    Refactored, added error handling, separated into functions

--- accountverify.py
+++ accountverify.py
@@ -6,74 +6,65 @@ from getpass import getpass
 from pprint import pprint
 import time
 
-# Load accounts from output.json
-if not os.path.exists("output.json"):
-    with open("output.json", "w") as file:
-        json.dump([], file)
+def load_json_file(file_path, default_value=[]):
+    """Loads a JSON file, if it doesn't exist, creates it with a default value."""
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as file:
+            json.dump(default_value, file)
+    with open(file_path, "r") as file:
+        return json.load(file)
 
-with open("output.json", "r") as file:
-    accounts = json.load(file)
+def save_json_file(file_path, data):
+    """Saves data to a JSON file."""
+    with open(file_path, "w") as file:
+        json.dump(data, file)
 
-# Check if the verified_accounts.json file exists, if not create and initialize it with an empty list
-if not os.path.exists("verified_accounts.json"):
-    with open("verified_accounts.json", "w") as file:
-        json.dump([], file)
-
-# Load verified accounts from verified_accounts.json
-with open("verified_accounts.json", "r") as file:
-    verified_accounts = json.load(file)
-
-# Shuffle the accounts randomly
-random.shuffle(accounts)
-
-for i, account in enumerate(accounts, 1):
-    print(account["username"])
-    
-    # Check if account is already verified
-    if account['username'] in verified_accounts:
-        print(f"Account {account['username']} already verified. Skipping...")
-        continue
-    
+def verify_account(account, verified_accounts):
+    """Verify a single Steam account."""
     user = wa.WebAuth(account["username"], account["password"])
     try:
         sess = user.login()
+        if sess is None:
+            raise ValueError("Failed to log in")
+
+        idcookie = sess.cookies.get_dict(domain="steamcommunity.com").get("sessionid")
+        if not idcookie:
+            raise ValueError("sessionid cookie not found")
+
+        resp = sess.post("https://store.steampowered.com/checkout/addfreelicense/303386", data={"sessionid": idcookie})
+        if resp is not None and "<h2>Success!</h2>" in resp.text:
+            print(f"Verified: {account['username']}, {account['password']}")
+            verified_accounts.add(account['username'])
+        else:
+            print("Account verification failed")
+
     except Exception as e:
-        print(f'Error: {e}')
-        continue
+        print(f'Error for account {account["username"]}: {e}')
 
-    if sess is None:
-        print("Error: Failed to log in")
-        continue
+def main():
+    accounts_file = "output.json"
+    verified_accounts_file = "verified_accounts.json"
 
-    idcookie = sess.cookies.get_dict(domain="steamcommunity.com")["sessionid"]
-    if idcookie is None or idcookie == "":
-        print("Error: sessionid cookie not found")
-        continue
+    accounts = load_json_file(accounts_file)
+    verified_accounts = set(load_json_file(verified_accounts_file))
 
-    resp = sess.post("https://store.steampowered.com/checkout/addfreelicense/303386", data={
-        "sessionid": idcookie
-    })
-    print(resp.status_code)
-    print(resp.text)
+    random.shuffle(accounts)
 
-    # Check if the verification was successful (adjust the condition based on your needs)
-    if resp is not None and "<h2>Success!</h2>" in resp.text:
-        # Do whatever you need to do with the verified account
-        # For example, you can print the username and password
-        print(f"Verified: {account['username']}, {account['password']}")
-        # Add the verified username to the list
-        verified_accounts.append(account['username'])
-        with open("verified_accounts.json", "w") as file:
-            json.dump(verified_accounts, file)
-    else:
-        # Account verification failed
-        print("Account verification failed")
+    for i, account in enumerate(accounts, 1):
+        if account['username'] in verified_accounts:
+            print(f"Account {account['username']} already verified. Skipping...")
+            continue
 
-    # Pause every 2 accounts and ask for user input to continue
-    if i % 2 == 0:
-        user_input = input("Continue? (y/n): ")
-        if user_input.lower() != "y":
-            break
+        verify_account(account, verified_accounts)
 
-    # Pause for 8 seconds before verifying the next account
-    time.sleep(8)
+        if i % 2 == 0:
+            user_input = input("Continue? (y/n): ")
+            if user_input.lower() != "y":
+                break
+
+        time.sleep(8)
+
+    save_json_file(verified_accounts_file, list(verified_accounts))
+
+if __name__ == "__main__":
+    main()
